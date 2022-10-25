ca0edb0a1a0d8560cde908a1c013e954fe5f267d
Martin Hebnes Pedersen <martin.h.pedersen@gmail.com>
2022-10-25 21:40:22 +0200

    Client v2

--- client/main.go
+++ client/main.go
@@ -4,12 +4,13 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"net/url"
 	"os"
 	"strings"
 	"time"
 )
 
-var DefaultClient = &http.Client{Timeout: time.Minute}
+var DefaultClient = &http.Client{Timeout: 5 * time.Minute}
 
 func init() { log.SetFlags(log.Ltime) }
 
@@ -24,7 +25,11 @@ func main() {
 			log.Fatal(err)
 		}
 	case "PRODUCE":
-		if err := produce(url, os.Stdout, os.Stdin); err != nil {
+		if len(os.Args) < 4 {
+			log.Fatal("Missing message")
+		}
+		r := strings.NewReader(strings.Join(os.Args[3:], " "))
+		if err := produce(url, os.Stdout, r); err != nil {
 			log.Fatal(err)
 		}
 	default:
@@ -33,14 +38,21 @@ func main() {
 }
 
 func usage() {
-	log.Fatal("Usage: %s (CONSUME or PRODUCE) (url)", os.Args[0])
+	log.Fatal("Usage: %s (CONSUME or PRODUCE) (url) [message]", os.Args[0])
 }
 
 func consume(url string, w io.Writer) error              { return doRequest("GET", url, w, nil) }
 func produce(url string, w io.Writer, r io.Reader) error { return doRequest("POST", url, w, r) }
 
-func doRequest(method, url string, w io.Writer, r io.Reader) error {
-	req, err := http.NewRequest(method, url, r)
+func doRequest(method, urlStr string, w io.Writer, r io.Reader) error {
+	url, err := url.Parse(urlStr)
+	if err != nil {
+		return err
+	}
+	if url.Scheme == "" && url.Host == "" {
+		url.Scheme, url.Host = "http", "localhost:8080"
+	}
+	req, err := http.NewRequest(method, url.String(), r)
 	if err != nil {
 		return err
 	}
